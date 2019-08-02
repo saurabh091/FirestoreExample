a9036ca0af3f257474768abd4bd10a87867b8287
Saurabh Rana <rana.saurabh@orangemantra.in>
2019-08-02 17:05:45 +0530

    Initial commit

---
+++ FirestoreExample/ViewController/HomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HomeViewController.swift
+//  FirestoreExample
+//
+//  Created by orangemac05 on 02/08/19.
+//  Copyright © 2019 Saurabh. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
